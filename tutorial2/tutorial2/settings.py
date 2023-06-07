f895a7d0721dcb1317eab59e787da5a0cce6e112
Shirley Yip <abc1c1sam@gmail.com>
2023-06-07 15:55:12 +0800

    Django REST

--- tutorial2/tutorial2/settings.py
+++ tutorial2/tutorial2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&brabn-u6hz#sodr$(1i@h4t)ti#!4p+8quf0nt_oi%y=n9bl9'
+SECRET_KEY = 'django-insecure-*u=j$3!i_#ku@drc9mw#r77qw9a26%u%#e633@o3$dcvako5ar'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'snippets',
 ]
 
 MIDDLEWARE = [
