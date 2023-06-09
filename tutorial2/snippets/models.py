1ea6dad29efc342ffdf49ddc27b97df45a3cbe01
Shirley Yip <abc1c1sam@gmail.com>
2023-06-09 12:22:13 +0800

    Django REST API

--- tutorial2/snippets/models.py
+++ tutorial2/snippets/models.py
@@ -2,6 +2,9 @@ from fileinput import lineno
 from django.db import models
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 
 # Create your models here.
@@ -11,15 +14,31 @@ STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 # define a table in the database
 class Snippet(models.Model):
+    # may be bugs here
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField(default='')
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length = 100, blank=True,default='')
     code = models.TextField()
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES,default='python',max_length=100)
     style = models.CharField(choices=STYLE_CHOICES,default='friendly',max_length=100)
+    
 
     class Meta:
         ordering  = ['created']
         db_table = 'snippets_snippet'
         def __str__(self):
             return self.created
+        
+    def save(self, *args, **kwargs):
+
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                                full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
+        
+        
--- tutorial2/tutorial2/urls.py
+++ tutorial2/tutorial2/urls.py
@@ -13,10 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 
+# api url
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('snippets.urls')),
+#     path('api-auth/', include('rest_framework.urls')),
+# ]
+
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from snippets import views
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet,basename="snippet")
+router.register(r'users', views.UserViewSet,basename="user")
+
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('snippets.urls')),
+    path('', include(router.urls)),
 ]
